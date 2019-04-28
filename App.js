4100077c72e7fb6e42194492bd4ca91a25042189
ricardo <ricardo21costa@hotmail.com>
2019-04-28 22:16:46 +0100

    Added couple os sliders

--- App.js
+++ App.js
@@ -13,35 +13,56 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 export default class App extends Component{
 
   constructor(props) {
-    super(props)
+    super(props)    
+    this.state = { initNW: 0 }
     this.state = { age: 18 }
    } 
 
-  getVal(val){
-    console.warn(val);
-    } 
+   // Console Debug message
+  // getVal(val){
+  //   console.warn(val);
+  //   } 
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Nativeeee!</Text>
+        <Text style={styles.welcome}>Compound Calculator!</Text>
+        
 
+        {/* Age */}
         <Slider
          style={{ width: 300 }}
          step={1}
-         minimumValue={18}
-         maximumValue={71}
+         minimumValue={0}
+         maximumValue={120}
          value={this.state.age}
          onValueChange={val => this.setState({ age: val })}
-         onSlidingComplete={ val => this.getVal(val)}
+        //  onSlidingComplete={ val => this.getVal(val)}
+        />       
+           
+
+        <Text style={styles.welcome}>
+        <Text style={{color: 'black'}}>Age: {this.state.age}</Text>          
+        </Text>  
+
+        {/* Initial Networth */}
+        <Slider
+         style={{ width: 300 }}
+         step={1}
+         minimumValue={0}
+         maximumValue={50000}
+         value={this.state.initNW}
+         onValueChange={val => this.setState({ initNW: val })}
+        //  onSlidingComplete={ val => this.getVal(val)}
         />
         <Text style={styles.welcome}>
-          {this.state.age}
-        </Text>            
+        <Text style={{color: 'black'}}>Networth: {this.state.initNW}</Text>          
+        </Text>  
+{/* 
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
-        </Text>
+        </Text> */}
 
       </View>
     );
