c7c4cb834eddd491a869cbbf0f6a72a592aa2725
ricardo <ricardo21costa@hotmail.com>
2019-04-29 23:16:41 +0100

    added MultiSliders

--- App.js
+++ App.js
@@ -21,34 +21,36 @@ export default class App extends Component{
 };
 
 sliderOneValuesChangeStart = () => {
-  this.setState({
-      sliderOneChanging: true,
-  });
+    this.setState({
+        sliderOneChanging: true,
+    });
 };
 
 sliderOneValuesChange = values => {
-  let newValues = [0];
-  newValues[0] = values[0];
-  this.setState({
-      sliderOneValue: newValues,
-  });
+    let newValues = [0];
+    newValues[0] = values[0];
+    this.setState({
+        sliderOneValue: newValues,
+    });
 };
 
 sliderOneValuesChangeFinish = () => {
-  this.setState({
-      sliderOneChanging: false,
-  });
+    this.setState({
+        sliderOneChanging: false,
+    });
 };
 
-  constructor(props) {
-    super(props)    
-    this.state = { multiSliderValue: [1,5]}
-    this.state = { initNW: 0 }
-    this.state = { age: 18 }
-    this.state = { testVar: 20 }
-    this.state = { leftValue: 20 }
-    this.state = { rightValue: 20 }
-   } 
+multiSliderValuesChange = values => {
+    this.setState({
+        multiSliderValue: values,
+    });
+};
+
+nonCollidingMultiSliderValuesChange = values => {
+    this.setState({
+        nonCollidingMultiSliderValue: values,
+    });
+};
  
 
    // Console Debug message
@@ -59,69 +61,81 @@ sliderOneValuesChangeFinish = () => {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Compound Calculator!</Text>        
+        <Text style={styles.welcome}>Compound Calculator!</Text>      
+        <Text style={styles.welcome}> </Text>    
 
-        <MultiSlider
-            values={this.state.sliderOneValue}
-            sliderLength={280}
-            onValuesChangeStart={this.sliderOneValuesChangeStart}
-            onValuesChange={this.sliderOneValuesChange}
-            onValuesChangeFinish={this.sliderOneValuesChangeFinish}
-        />
+         {/* Initial Networth */}
 
-        <Text style={styles.welcome}>
-        <Text style={{color: 'black'}}>testVar: {this.state.sliderOneValue}</Text>          
-        </Text>
+         <Text
+                            style={[
+                                styles.text,
+                                this.state.sliderOneChanging && { color: 'red' },
+                            ]}
+                        >
+                            {this.state.sliderOneValue}
+                        </Text>
+
+         <MultiSlider
+                        values={this.state.sliderOneValue}
+                        sliderLength={280}
+                        onValuesChangeStart={this.sliderOneValuesChangeStart}
+                        onValuesChange={this.sliderOneValuesChange}
+                        onValuesChangeFinish={this.sliderOneValuesChangeFinish}
+                    />
+
+         <View style={styles.sliderOne}>
+                        <Text style={styles.text}>Two Markers:</Text>
+                        <Text style={styles.text}>{this.state.multiSliderValue[0]} </Text>
+                        <Text style={styles.text}>{this.state.multiSliderValue[1]}</Text>
+                    </View>
+                    <MultiSlider
+                        values={[
+                            this.state.multiSliderValue[0],
+                            this.state.multiSliderValue[1],
+                        ]}
+                        sliderLength={280}
+                        onValuesChange={this.multiSliderValuesChange}
+                        min={0}
+                        max={10}
+                        step={1}
+                        allowOverlap
+                        snapped
+                    />
+
+       
+
+        {/* <Text style={styles.welcome}>
+        <Text style={{color: 'black'}}>Age: {this.state.sliderOneValue}</Text>          
+        </Text> */}
 
         {/* Age */}
-        <Slider
+        {/* <Slider
          style={{ width: 300 }}
          step={1}
          minimumValue={0}
          maximumValue={120}
          value={this.state.age}
          onValueChange={val => this.setState({ age: val })}
-        //  onSlidingComplete={ val => this.getVal(val)}
-        />       
-           
-
-        <Text style={styles.welcome}>
-        <Text style={{color: 'black'}}>Age: {this.state.age}</Text>          
-        </Text>  
-
-        {/* Initial Networth */}
-        <Slider
-         style={{ width: 300 }}
-         step={1}
-         minimumValue={0}
-         maximumValue={50000}
-         value={this.state.initNW}
-         onValueChange={val => this.setState({ initNW: val })}
-        //  onSlidingComplete={ val => this.getVal(val)}
-        />
-        <Text style={styles.welcome}>
-        <Text style={{color: 'black'}}>Networth: {this.state.initNW}</Text>          
-        </Text>  
+        //  onSlidingComplete={ val => this.getVal(val)} */}
+        {/* />        */}           
+      
 
          {/* Initial Networth */}
-         <Slider
-         style={{ width: 300 }}
-         step={1}
-         minimumValue={0}
-         maximumValue={50000}
-         value={this.state.initNW}
-         onValueChange={val => this.setState({ testVar: val })}
-        //  onSlidingComplete={ val => this.getVal(val)}
-        />
+        {/* <MultiSlider            
+          step={1}
+          sliderLength={280}
+          minimumValue={0}
+          maximumValue={120}
+          value={this.state.initNW}
+          // onValuesChangeStart={this.sliderOneValuesChangeStart}
+          onValuesChange={val => this.setState({ initNW: val })}
+          // onValuesChangeFinish={this.sliderOneValuesChangeFinish}
+        />             
+       
         <Text style={styles.welcome}>
-        <Text style={{color: 'black'}}>testVar: {this.state.testVar}</Text>          
-        </Text>  
-
-        {/* 
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text> */}
+        <Text style={{color: 'black'}}>Networth: {this.state.initNW}</Text>          
+        </Text>   */}
+     
 
       </View>
     );
@@ -140,9 +154,18 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  text: {
+    alignSelf: 'center',
+    paddingVertical: 20,
+  },
+  sliderOne: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+},
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
+  
 });
