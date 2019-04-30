4c3649ea27ce950485da8d4f0a998e796e52f951
Ricardo Costa <Ricardo.Costa@millbrookindustries.co.uk>
2019-04-30 12:37:09 +0100

    Progress around MultiSliders

--- App.js
+++ App.js
@@ -10,20 +10,13 @@ import React, {Component} from 'react';
 import MultiSlider from '@ptomasroos/react-native-multi-slider';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component {
   
   state = {
     sliderOneChanging: false,
-    sliderOneValue: [5],
-    multiSliderValue: [3, 7],
-    nonCollidingMultiSliderValue: [0, 100],
+    sliderOneValue: [0],
+    nonCollidingMultiSliderValue: [25, 45],
 };
 
 sliderOneValuesChangeStart = () => {
@@ -46,12 +39,6 @@ sliderOneValuesChangeFinish = () => {
     });
 };
 
-multiSliderValuesChange = values => {
-    this.setState({
-        multiSliderValue: values,
-    });
-};
-
 nonCollidingMultiSliderValuesChange = values => {
     this.setState({
         nonCollidingMultiSliderValue: values,
@@ -61,47 +48,49 @@ nonCollidingMultiSliderValuesChange = values => {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.instructions}>{instructions}</Text> */}
 
-        <Text
-                            style={[
-                                styles.text,
-                                this.state.sliderOneChanging && { color: 'red' },
-                            ]}
-                        >
-                            {this.state.sliderOneValue}
-                        </Text>
+        <View style={styles.sliderOne}>
+            <Text style={styles.text}>Initial Networth: </Text>  
+            <Text style={styles.text}>{this.state.sliderOneValue} </Text>         
+        </View>
 
          <MultiSlider
-                        values={this.state.sliderOneValue}
-                        sliderLength={280}
-                        onValuesChangeStart={this.sliderOneValuesChangeStart}
-                        onValuesChange={this.sliderOneValuesChange}
-                        onValuesChangeFinish={this.sliderOneValuesChangeFinish}
-                    />
+            values={this.state.sliderOneValue}
+            sliderLength={280}
+            min={0}
+            max={100000}
+            step={1}
+            onValuesChangeStart={this.sliderOneValuesChangeStart}
+            onValuesChange={this.sliderOneValuesChange}
+            onValuesChangeFinish={this.sliderOneValuesChangeFinish}
+            allowOverlap
+            snapped
+          />
 
-         <View style={styles.sliderOne}>
-                        <Text style={styles.text}>Two Markers:</Text>
-                        <Text style={styles.text}>{this.state.multiSliderValue[0]} </Text>
-                        <Text style={styles.text}>{this.state.multiSliderValue[1]}</Text>
-                    </View>
-                    <MultiSlider
-                        values={[
-                            this.state.multiSliderValue[0],
-                            this.state.multiSliderValue[1],
-                        ]}
-                        sliderLength={280}
-                        onValuesChange={this.multiSliderValuesChange}
-                        min={0}
-                        max={10}
-                        step={1}
-                        allowOverlap
-                        snapped
-                    />
+        <View style={styles.sliderOne}>
+            <Text style={styles.text}>Age: [</Text>
+            <Text style={styles.text}>{this.state.nonCollidingMultiSliderValue[0]} </Text>
+            <Text style={styles.text}> - {this.state.nonCollidingMultiSliderValue[1]}]</Text>
+        </View>
 
+        <MultiSlider
+            values={[
+                this.state.nonCollidingMultiSliderValue[0],
+                this.state.nonCollidingMultiSliderValue[1],
+            ]}
+            sliderLength={280}
+            onValuesChange={this.nonCollidingMultiSliderValuesChange}
+            min={0}
+            max={100}
+            step={1}
+            allowOverlap
+            snapped
+        />
 
+       
       </View>
     );
   }
