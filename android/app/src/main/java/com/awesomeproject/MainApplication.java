7ed46bbe4f54a767f460c339bfc073cbc2914013
Ricardo Costa <Ricardo.Costa@millbrookindustries.co.uk>
2019-06-20 15:50:54 +0100

    Fixed android components related bug

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.awesomeproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import kim.taegon.rnintl.ReactNativeIntlPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.kishanjvaghela.cardview.RNCardViewPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeIntlPackage(),
             new VectorIconsPackage(),
             new RNCardViewPackage()
       );
--- index.js
+++ index.js
@@ -5,5 +5,14 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { Platform } from 'react-native';
+
+if (Platform.OS === 'android') {
+  require('intl');
+  require('intl/locale-data/jsonp/fr-BE');
+  require('intl/locale-data/jsonp/nl-BE');
+  require('intl/locale-data/jsonp/it-IT');
+  require('intl/locale-data/jsonp/ja-JP');
+}
 
 AppRegistry.registerComponent(appName, () => App);
