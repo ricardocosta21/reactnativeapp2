0bd69b5c41bfd09d3d27e8f4a654d3d3f2f4cd91
Ricardo Costa <ricardo21costa@hotmail.com>
2020-04-03 16:30:20 +0100

    hopefully fixed app's name. wasn't showing on google play store before

--- src/About.js
+++ src/About.js
@@ -24,7 +24,7 @@ const LIGHT_GRAY = '#ECECEC';
 const WHITE = '#FFFFFF';
 const TRANSPARENT = 'rgba(52, 52, 52, alpha)';
 
-const appVersion = '1.0';
+const appVersion = '1.1';
 
 export default class About extends Component {
   render() {
